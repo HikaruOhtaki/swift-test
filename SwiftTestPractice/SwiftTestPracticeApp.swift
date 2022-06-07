52a285291f53178822491176c8257cde4bf24979
HikaruOhtaki <hikaru.11.oth@icloud.com>
2022-06-07 23:54:30 +0900

    first commit

---
+++ SwiftTestPractice/SwiftTestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestPracticeApp.swift
+//  SwiftTestPractice
+//
+//  Created by ohtaki hikaru on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
